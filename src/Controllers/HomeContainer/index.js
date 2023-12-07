a2dfef878ec88732e8f7ec09cfb30695ef46aef2
irfan5858 <irfan@roughcopies.com>
2023-12-07 21:32:11 +1100

    container  and  screen structure added.

---
+++ src/Controllers/HomeContainer/index.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import HomeScreen from "../../Screens/HomeScreen";
+
+const HomeContainer = ()=> {
+    return  (
+   <HomeScreen />
+    )
+}
+export default HomeContainer
